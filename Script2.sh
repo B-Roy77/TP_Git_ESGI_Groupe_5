0a7267cdacb22acd536618d4c2523e273ce4e101
Alexis <101020908+Juanito2053@users.noreply.github.com>
2022-09-29 11:24:06 +0200

    Seconde version du script 2

--- Script2.sh
+++ Script2.sh
@@ -3,13 +3,7 @@
 ##Variable :
 repository="https://github.com/KeligMartin/4-SRC.git"
 
-###Aller sur le disque C: du PC
-cd C:\
-#Création du fichier sur le Disque C: sur windows
-mkdir TP_Git_ESGI_Groupe_5
-#localFolder="C:\Users\Alexis\Downloads\TP2222"
 ### initialisation du fichier
 git init
 ### Clonage du repo
 git clone $repository
-#$localFolder
