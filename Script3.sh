d21e5316e6ba5fe7ffe727b3d2e18b316d64fe25
Alexis <101020908+Juanito2053@users.noreply.github.com>
2022-09-29 12:13:59 +0200

    Troisième version avec davantage de modification

--- Script3.sh
+++ Script3.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
-addition(){
-   sum=$(($1+$2))
-   return $sum
-}
-read -p "Entrez un numéro : " int1
-read -p "Entrez un numéro : " int2
-add $int1 $int2
-echo "Le résultat est : " $?
+# !/bin/bash
+ 
+echo "Enter Two numbers : "
+read a
+read b
+ 
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+ 
+
+case $ch in
+  1)res=`echo $a + $b | bc`
+  ;;
+  2)res=`echo $a - $b | bc`
+  ;;
+  3)res=`echo $a \* $b | bc`
+  ;;
+  4)res=`echo "scale=2; $a / $b" | bc`
+  ;;
+esac
+echo "Result : $res"
